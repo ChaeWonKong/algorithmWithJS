a56dddaaa2f91b3ffc07d39e96d9bbe0d76dfd13
공채원 <chaewonkong@gmail.com>
2018-12-27 15:49:24 +0900

    Complete combinations

--- combinations.js
+++ combinations.js
@@ -5,12 +5,14 @@ Return all possible combinations of N number of item,
 for given array. 
 */
 
-const arr = [1, 2, 3, 4];
+const arr = [1, 2, 2, 3];
 const N = 3;
 
 function getAllCombinations(arr, N) {
   const combinations = [];
   const picked = [];
+  const used = [];
+  for (item of arr) used.push(0);
 
   function find(picked) {
     if (picked.length === N) {
@@ -23,9 +25,13 @@ function getAllCombinations(arr, N) {
     } else {
       let start = picked.length ? picked[picked.length - 1] + 1 : 0;
       for (let i = start; i < arr.length; i++) {
-        picked.push(i);
-        find(picked);
-        picked.pop();
+        if (i === 0 || arr[i] !== arr[i - 1] || used[i - 1]) {
+          picked.push(i);
+          used[i] = 1;
+          find(picked);
+          picked.pop();
+          used[i] = 0;
+        }
       }
     }
   }
